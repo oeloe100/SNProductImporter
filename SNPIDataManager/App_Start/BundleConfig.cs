ce3774e293abf5447470d0cb554353cf17a6344b
Anton <tonraschenko@hotmail.com>
2020-05-09 17:10:44 +0200

    Updated Jquery version (Minor) + Created MVC Pages
    Created MVC pages + jQUERY version update + Customized navigation CSS / Created Directories for Api & for MVC controllers

--- SNPIDataManager/App_Start/BundleConfig.cs
+++ SNPIDataManager/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace SNPIDataManager
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Custom.css"));
         }
     }
 }
