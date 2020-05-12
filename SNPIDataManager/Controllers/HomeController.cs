92930c5aa2647c1de4566ed53f951d2e81d32b53
Anton <tonraschenko@hotmail.com>
2020-05-12 21:16:07 +0200

    Not Functional Commit
    Day final commit. Working for. Nullreference problem occurs, needs to get fixed

--- SNPIDataManager/Controllers/HomeController.cs
+++ SNPIDataManager/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SNPIDataManager.Helpers;
+using SNPIDataManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,40 @@ namespace SNPIDataManager.Controllers
 {
     public class HomeController : Controller
     {
+        private IAPIHelper _apiHelper;
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(AuthenticatedUser user)
+        {
+            var result = await _apiHelper.Authenticate(user.Username, user.Access_Token);
+
+            Console.WriteLine(result.Access_Token);
+
+            Console.WriteLine();
+
+            return await Task.Run(() => View(user));
+        }
+
+        /*
+        public async Task<AuthenticatedUser> Login()
+        {
+            var result = await _apiHelper.Authenticate(_username, _password);
+
+            var authenticatedUser = new AuthenticatedUser
+            {
+                Username = result.Username,
+                Access_Token = result.Access_Token
+            };
+
+            return authenticatedUser;
+        }
+        */
     }
 }
---
+++ SNPIDataManager/Models/AuthenticatedUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SNPIDataManager.Models
+{
+    public class AuthenticatedUser
+    {
+        public string Access_Token { get; set; }
+        public string Username { get; set; }
+    }
+}
