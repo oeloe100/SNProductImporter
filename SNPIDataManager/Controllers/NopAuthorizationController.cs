8046aae6da34e384c8817fa87c9a308b176f4c28
Anton <tonraschenko@hotmail.com>
2020-05-26 09:41:51 +0200

    added Authorization. now working on access.

--- SNPIDataManager/Controllers/NopAuthorizationController.cs
+++ SNPIDataManager/Controllers/NopAuthorizationController.cs
@@ -1,7 +1,9 @@
-﻿using SNPIDataManager.Models.NopAuthorizationModels;
+﻿using SNPIDataManager.Managers;
+using SNPIDataManager.Models.NopAuthorizationModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Web;
 using System.Web.Http.Results;
@@ -25,7 +27,27 @@ namespace SNPIDataManager.Controllers
             {
                 try
                 {
-                    throw new NotImplementedException();
+                    var nopAuthorizationManager = new NopAuthorizationManager(model.ClientId, model.ClientSecret, model.ServerUrl);
+                    var callbackUrl = Url.RouteUrl("TokenEP", null, Request.Url.Scheme);
+
+                    if (callbackUrl != model.RedirectUrl)
+                    {
+                        return BadRequest();
+                    }
+
+                    // *** CURRENT SESSION INSTANCE ***//
+                    Session["clientId"] = model.ClientId;
+                    Session["clientSecret"] = model.ClientSecret;
+                    Session["serverUrl"] = model.ServerUrl;
+                    Session["redirectUrl"] = callbackUrl;
+
+                    //*** DONT SAVE ANYWHERE ***//
+                    var state = Guid.NewGuid();
+                    Session["state"] = state;
+
+                    var authorizationUrl = nopAuthorizationManager.GetAuthorizationUrl(callbackUrl, new string[] { }, state.ToString());
+
+                    return Redirect(authorizationUrl);
                 }
                 catch (Exception ex)
                 {
@@ -36,7 +58,15 @@ namespace SNPIDataManager.Controllers
             return BadRequest();
         }
 
-        public ActionResult BadRequest(string message = "Bad Request")
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult ReceiveAccessToken(string code, string state)
+        {
+            Console.WriteLine();
+            throw new NotImplementedException();
+        }
+
+        private ActionResult BadRequest(string message = "Bad Request")
         {
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest, message);
         }
