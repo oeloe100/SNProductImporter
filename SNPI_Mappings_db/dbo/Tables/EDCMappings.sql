341fc47e2a541fb61006746e7d6ed9cace777113
Anton <tonraschenko@hotmail.com>
2020-06-29 11:43:08 +0200

    Created Database for Mappings

---
+++ SNPI_Mappings_db/dbo/Tables/EDCMappings.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EDCMappings]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [shopCategory] NVARCHAR(50) NOT NULL, 
+    [supplierCategory] NVARCHAR(50) NOT NULL, 
+    [shopCategoryId] NVARCHAR(50) NOT NULL, 
+    [SupplierCategoryId] NVARCHAR(50) NOT NULL
+)
